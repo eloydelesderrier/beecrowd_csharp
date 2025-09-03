4ccadc2b6be57e2f140abccd0b4e773ce5396a04
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-09-03 15:18:24 -0300

    Area

---
+++ Desafios/Area.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beecrowd_c_.Desafios
+{
+    public class Area
+    {
+        public static void Main(string[] args)
+        { 
+            string[] valores = Console.ReadLine().Split(' ');
+            double A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(valores[1], CultureInfo.InvariantCulture );
+            double C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            double pi = 3.14159;
+
+            double areaTriangulo = (A * C) / 2;
+            double areaCirculo = pi * Math.Pow(C, 2);
+            double areaTrapezio = ((A + B) * C) / 2.0;
+            double areaQuadrado = Math.Pow(B, 2);
+            double areaRetangulo = A * B;
+
+            Console.WriteLine($"TRIANGULO: {areaTriangulo.ToString("F3")}");
+            Console.WriteLine($"CIRCULO: {areaCirculo.ToString("F3")}");
+            Console.WriteLine($"TRAPEZIO: {areaTrapezio.ToString("F3")}");
+            Console.WriteLine($"QUADRADO: {areaQuadrado.ToString("F3")}");
+            Console.WriteLine($"RETANGULO: {areaRetangulo.ToString("F3")}");
+        }
+    }
+}
--- obj/Debug/net9.0/beecrowd_c#.AssemblyInfo.cs
+++ obj/Debug/net9.0/beecrowd_c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beecrowd_c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaca4ec56e48125b3ad7682c21d5fbb7712ddcaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21d333da8e75048f3e4d2466664a29afe845335")]
 [assembly: System.Reflection.AssemblyProductAttribute("beecrowd_c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beecrowd_c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
